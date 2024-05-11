bb88b6a7b81d00ba410437c2b05c554996c1365d
SUPRIYA  PRABHAT <78290285+supriyaprabhat@users.noreply.github.com>
2024-05-12 02:26:04 +0530

    Create code.php

---
+++ fitindiaclub/code.php
@@ -0,0 +1,48 @@
+
+<?php  
+    // Check if the form is submitted 
+    if ( isset( $_POST['submit'] ) ) { // retrieve the form data by using the element's name attributes value as key 
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $pincode = $_POST['pincode']; 
+        
+        $curl = curl_init();
+    
+        curl_setopt($curl, CURLOPT_URL, 'https://www.privyr.com/api/v1/incoming-leads/0vZfjMQw/HQBFKSA9#generic-webhook');
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+        
+        $data = array(
+          'name' => $name,
+          'email' => $email,
+          'phone' => $phone,
+          'pincode' => $pincode
+        );
+        
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        
+        // $response = curl_exec($curl);
+        // curl_close($curl);
+        
+        // echo $response;
+        
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        
+        $response = curl_exec($curl);
+        
+        // Check if cURL request was successful and response is as expected
+        if ($response !== false) {
+            // Redirect to thank you page if cURL request was successful
+            header("Location: thanks.html");
+            exit();
+        } else {
+            // Handle the case if cURL request fails
+            echo "Error occurred. Please try again.";
+        }
+        
+        curl_close($curl);
+        
+        
+    }
+?> 
